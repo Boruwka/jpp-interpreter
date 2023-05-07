1b739b36c96ee1fcff7679790c2d2c624d24aec5
Jagoda Bracha <jb429153@students.mimuw.edu.pl>
2023-05-07 12:04:31 +0200

    skompilowalo sie

--- interpreter.hs
+++ interpreter.hs
@@ -6,19 +6,25 @@ import Control.Monad.Identity
 import Control.Monad.State
 import Control.Monad.Trans.Class
 
-import Grammar.Abs
-import Grammar.Par
+import System.IO ( stdin, hGetContents )
+import System.Environment ( getArgs, getProgName )
+import System.Exit ( exitFailure, exitSuccess )
+
 import Grammar.Lex
+import Grammar.Print
+import Grammar.Abs
+import Grammar.ErrM
+import Grammar.Par
 
 
 type Loc = Integer
 type Env = Map.Map Ident Loc
-type Err = String
+type ErrMess = String
 type Store = Map.Map Loc Integer
 type StoreLocations = (Store, Integer)
 type Type = String
 
-type Interpreter a = (ReaderT Env (ExceptT Err (StateT StoreLocations Identity))) a
+type Interpreter a = (ReaderT Env (ExceptT ErrMess (StateT StoreLocations Identity))) a
 
 evalExpr :: Expr -> Interpreter Integer 
 
@@ -44,16 +50,18 @@ evalExpr (EAdd _ e1 e2) = do
     
 evalProg :: Program -> Interpreter ()
 
-evalProg (Prog _ insts) = evalInsts insts
+evalProg (Prog _ insts) = do
+    evalInsts insts
+    return ()
     
-evalInsts :: [Inst] -> Interpreter ()
+evalInsts :: [Inst] -> Interpreter (Env -> Env)
 
-evalInsts [] = return ()
+evalInsts [] = return id
 evalInsts (inst:tl) = do
-    evalInst inst
-    evalInsts tl
+    env_modifier <- evalInst inst
+    local env_modifier (evalInsts tl)
     
-evalInst :: Inst -> Interpreter ()
+evalInst :: Inst -> Interpreter (Env -> Env)
     
 evalInst (IAssign _ x e) = do 
     env <- ask
@@ -63,6 +71,7 @@ evalInst (IAssign _ x e) = do
         if Map.member l store then do
             n <- (evalExpr e)
             put ((Map.insert l n store), loc)
+            return id
         else
             throwError ("Weird error - localisation not known in state!")
     else
@@ -77,27 +86,47 @@ evalInst (IIf _ e i1 i2) = do
 
 evalInst (IInit _ t x e) = do
     n <- (evalExpr e)
-    env_v <- ask
+    env <- ask
     (store, loc) <- get
-    local (const env_v) (Map.insert x loc env_v)
+    --local (const (Map.insert x loc env_v))
     put ((Map.insert loc n store), (loc + 1)) 
+    return (\env -> (Map.insert x loc env))
 
 
-progTree = undefined -- trzeba przepuścić przez parser
 
-runInterpreter :: (Interpreter a) -> Env -> Store -> Either Err a
-runInterpreter monad env store = runIdentity (runExceptT (runStateT (runReaderT monad env) store))
+runInterpreter :: (Interpreter a) -> Env -> StoreLocations -> (Either ErrMess a, StoreLocations)
+runInterpreter monad env store = runIdentity (runStateT (runExceptT (runReaderT monad env)) store)
 
-interpret progTree = runInterpreter (evalProg progTree) Map.empty (Map.empty, 0)
+interpret :: Program' BNFC'Position -> (Either ErrMess (), StoreLocations)
+interpret progTree = runInterpreter (evalProg progTree) (Map.empty) ((Map.empty), 0)
 
-interpretFile :: FilePath -> IO ()
+{-interpretFile :: FilePath -> IO ()
 interpretFile file = readFile file >>= interpretText
 
 interpretText :: String -> IO ()
-interpretText input = interpret parsedTokens
+interpretText input = print (show (interpret progTree))
   where
     tokens = myLexer input
-    parsedTokens = pProgram tokens
+    progTree = pProgram tokens-}
+    
+type PInfo = Maybe (Int, Int)
+
+type ParseFun a = [Token] -> Err a
+
+--myLLexer = myLexer
+
+runFile :: ParseFun Program -> FilePath -> IO ()
+runFile p f = readFile f >>= run p
+
+run :: ParseFun Program -> String -> IO ()
+run p s = let ts = myLexer s in case p ts of
+            Bad s   -> do
+                        putStrLn "Interpreter failed to parse the program"
+                        exitFailure
+            Ok tree -> do
+                        print (show (interpret tree))
+                        exitSuccess
+
 
 path = "przyklady/przyklad1.txt"
-main = interpretFile path
+main = runFile pProgram path
